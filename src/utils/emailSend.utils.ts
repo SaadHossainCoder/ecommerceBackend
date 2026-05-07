d2cca1721264b39084a23336fe074f08abee4097
Saad Hossain <hossainsaad324@gmail.com>
2026-05-07 11:12:58 +0530

    feat: implement nodemailer utility for SMTP configuration and email dispatching

--- src/utils/emailSend.utils.ts
+++ src/utils/emailSend.utils.ts
@@ -11,13 +11,16 @@ const isSMTPConfigured = () => {
 };
 
 const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST,
+  // host: process.env.SMTP_HOST,
   service: "gmail",
-  port: Number(process.env.SMTP_PORT),
-  secure: Number(process.env.SMTP_PORT) === 465, // ✅ auto-detect
+  // port: Number(process.env.SMTP_PORT),
+  // secure: Number(process.env.SMTP_PORT) === 465, // ✅ auto-detect
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
+  },
+   tls: {
+    rejectUnauthorized: false,         // Required for Render's outbound connections
   },
 });
 // const transporter = nodemailer.createTransport({
@@ -60,7 +63,7 @@ export const sendEmail = async (
     }
 
     const info = await transporter.sendMail({
-      from: `"Ecommerce App" <${process.env.SMTP_USER}>`,
+      from: `"khoshil" <${process.env.SMTP_USER}>`,
       to,
       subject,
       html,
